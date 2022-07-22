065759f366d92642f7b6491103275e391f13bfe4
AlamoVinicius <alamo.cma@gmail.com>
2022-07-21 22:49:39 -0300

    theme implemented

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+
+import { THEME } from "./src/screens/styles/theme";
+
 import SignIn from "./src/screens/SignIn";
 
-const App = () => <SignIn />;
+const App = () => (
+  <NativeBaseProvider theme={THEME}>
+    <SignIn />
+  </NativeBaseProvider>
+);
 
 export default App;
-
