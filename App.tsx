98860459049a116316d4e0012d30ff98d0d8b148
AlamoVinicius <alamo.cma22@gmail.com>
2022-07-24 20:33:52 -0300

    feat: fonts and styles

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import { THEME } from "./src/screens/styles/theme";
-
+// ====== screens ======
 import SignIn from "./src/screens/SignIn";
+// ====== components ======
+import Loading from "./src/COMPONENTS/Loading";
 
-const App = () => (
-  <NativeBaseProvider theme={THEME}>
-    <SignIn />
-  </NativeBaseProvider>
-);
+const App = () => {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+};
 
 export default App;
-
 
 // podemos sobrepor o theme do nativebase com o nosso tema customizado
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { VStack, Heading } from "native-base";
 
+import Logo from "../ASSETS/logo_primary.svg";
+
 const SignIn = () => {
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
